90a6fcb8616f9271a38358b352463cecab4dbb81
Michael Cheng <mcheng.work@gmail.com>
2017-07-29 08:18:38 +0000

    Devise related changes - Removed registerable - Added authentication to all pages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'hello' do
+      it 'has valid email' do
+          user = User.new(email: "michael@example.com", password: "hello123")
+
+          expect(user).to be_valid
+      end
+  end
 end
