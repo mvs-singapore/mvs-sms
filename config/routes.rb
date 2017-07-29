460391aa8c3ed67896a4b463a6a670af31e23e78
Michael Cheng <mcheng.work@gmail.com>
2017-07-29 07:34:11 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'pages#index'
+  get 'pages/index'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
