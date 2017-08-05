f7bea5b1565c9e0cdd0ebeaa7adda815e7733bf4
Michael Cheng <pair+michael@coderkungfu.com>
2017-08-06 00:02:11 +0800

    Show error messages when login failed / succeeded.
    [#149345205]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+        success: "alert-success",
+        error: "alert-error",
+        alert: "alert-danger",
+        notice: "alert-info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def bootstrap_glyphs_icon(flash_type)
+    {
+        success: "glyphicon-ok",
+        error: "glyphicon-exclamation-sign",
+        alert: "glyphicon-warning-sign",
+        notice: "glyphicon-info-sign"
+    }[flash_type.to_sym] || 'glyphicon-screenshot'
+  end
 end
