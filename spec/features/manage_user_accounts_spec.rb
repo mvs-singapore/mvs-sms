f348d61f5549459b4b5b6400098bfcef70d44f98
Lian Tong Wei and Michael Cheng <pair+lian+michael@techladies.co>
2017-08-09 12:15:15 +0800

    Add feature test for admin to create user [#149345205]

--- spec/features/manage_user_accounts_spec.rb
+++ spec/features/manage_user_accounts_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 describe 'manage user accounts', type: :feature do
-  let!(:user) { User.create(email: 'user@example.com', password: 'password', role: 'super_admin') }
+  let!(:super_user) { User.create(email: 'admin@example.com', password: 'password', role: 'super_admin') }
 
-  it 'signs me in', js: true do
+  it 'signs me in' do
     visit '/users/sign_in'
 
     within('#new_user') do
-      fill_in 'Email', with: user.email
+      fill_in 'Email', with: super_user.email
       fill_in 'Password', with: 'password'
     end
     click_button 'Log in'
 
     expect(page).to have_text 'Signed in successfully'
+  end
+
+  xdescribe 'new user' do
+    it 'creates new user with role' do
+      sign_in super_user
+
+      visit '/admin/users/'
+      click_link 'New User'
+
+      within('#new_user') do
+        fill_in 'Email', with: 'teacher@example.com'
+        fill_in 'Name', with: 'Yammy'
+        select('Teacher', :from => 'User Role')
+      end
+      click_button 'Create User'
+
+      expect(page).to have_text 'Successfully created user.'
+
+      #TODO Ensure email was sent.
+    end
   end
 end
