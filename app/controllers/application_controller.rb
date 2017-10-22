becb77be5bb0f9420cf1e10224bb3a5b3600d98e
Pearly Ong <ong.pearly@gmail.com>
2017-10-22 11:39:44 +0800

    Set up tracking for student status [#150379344]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  before_action :set_paper_trail_whodunnit
 
   helper_method :is_admin?
 
@@ -18,6 +19,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     update_attrs = [:password, :password_confirmation, :current_password, :email, :name]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+  end
+
+  def user_for_paper_trail
+    user_signed_in? ? current_user.name : 'Unknown User'
   end
 
 end
