71500d9cd268bee12fcccb8de9aa69a3b235fcaf
Michael Cheng <pair+michaelccm@techladies.co>
2018-12-01 15:11:46 +0800

    Remove field validation for referred_by and start & end date for past edu

--- spec/models/past_education_record_spec.rb
+++ spec/models/past_education_record_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe PastEducationRecord, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:school_name) }
-    it { is_expected.to validate_presence_of(:from_date) }
-    it { is_expected.to validate_presence_of(:to_date) }
   end
 
   describe 'associations' do
