3b5e543e4aa538ef92b34d98bd1d3da6cfd7b875
jeanwei <jeanwei938@msn.com>
2018-12-09 17:30:01 +0800

    Update sample csv to import_students view

--- app/controllers/admin/import_students_controller.rb
+++ app/controllers/admin/import_students_controller.rb
@@ -5,8 +5,7 @@ class Admin::ImportStudentsController < ApplicationController
     raise Exception.new('No file is uploaded') unless params["csv_upload_file"]
     input = params["csv_upload_file"]
     raise Exception.new('Please upload CSV file') if input.content_type != 'text/csv'
-    
-    StudentsImporter.new(filepath: input.tempfile.path)
+    StudentsImporter.new(input.tempfile.path).execute
     redirect_to admin_import_students_path, notice: 'Import success'
   rescue => e
     redirect_to admin_import_students_path, alert: e.message
