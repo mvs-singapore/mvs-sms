45edec6d8b87b35407b7fcc4e061614d5ceef21c
Michael Cheng <pair+michaelccm@techladies.co>
2018-12-15 10:17:21 +0800

    Add default mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
       port: ENV['SMTP_PORT'],
       authentication: ENV['SMTP_AUTH'].to_sym
   }
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_MAILER_HOST'] }
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
